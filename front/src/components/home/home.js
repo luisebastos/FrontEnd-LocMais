5f2471eabae927df7d5058db36d6f5df4cfb6ec3
luisebastos <luise@gustavobastos.com.br>
2025-01-26 10:42:25 -0300

    texto_home

--- front/src/components/home/home.js
+++ front/src/components/home/home.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Sidebar from '../sidebar/sidebar';
-import Topbar from '../topbar/topbar'; 
+import Topbar from '../topbar/topbar';
+import './home.css'; // Certifique-se que o caminho está correto
 
 const Home = () => {
   return (
     <div className="home-page">
-      <Topbar /> 
-      <Sidebar /> 
-      <div className="home-content">
-        {/* conteudo pagina Home */}
+      <Topbar />
+      <div className="main-content">
+        <Sidebar />
+        <div className="content-container">
+          <div className="home-content">
+            <h2 className="welcome-message">Bem vindo, Administrador!</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
-
-
