9784b1f28023994b42d71566531d0283475f4072
luisebastos <luise@gustavobastos.com.br>
2025-01-27 21:39:12 -0300

    editar funcionalidade

--- front/src/components/usuarios/usuario_id.js
+++ front/src/components/usuarios/usuario_id.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import Sidebar from '../sidebar/sidebar';
 import Topbar from '../topbar/topbar';
 import './usuario_id.css'; 
+import usuariosData from '../../data_teste/usuarios.json';
 
 const Usuario_id = () => {
+  const { id } = useParams(); // Captura o ID do URL
+  const [usuario, setUsuario] = useState(null);
+
+  useEffect(() => {
+    const usuarioEncontrado = usuariosData.find(user => user.id === id);
+    setUsuario(usuarioEncontrado);
+  }, [id]);
+
   return (
     <div className="usuarioid-page">
       <Topbar />
@@ -11,7 +21,24 @@ const Usuario_id = () => {
         <Sidebar />
         <div className="content-container">
           <div className="usuarioid-content">
-            <h2 className="usuarioid-message">Usuario_id</h2>
+            {usuario ? (
+              <>
+                <h2 className="usuarioid-message">Usuário: {usuario.nome}</h2>
+                <table className="usuario-details-table">
+                  <tbody>
+                    {Object.entries(usuario).map(([key, value]) => (
+                      key !== 'id' && <tr key={key}>
+                        <th>{key.charAt(0).toUpperCase() + key.slice(1)}</th>
+                        <td>{value.toString()}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <Link to={`/usuarios/editar/${usuario.id}`} className="edit-button">Editar</Link>
+              </>
+            ) : (
+              <p>Usuário não encontrado</p>
+            )}
           </div>
         </div>
       </div>
@@ -20,3 +47,4 @@ const Usuario_id = () => {
 };
 
 export default Usuario_id;
+
